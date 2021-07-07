b62ae82ff89930025c750696db4d1e076d66d508
shinichi <76652303+shinichi1729@users.noreply.github.com>
2021-07-07 15:54:41 +0900

    Create 085.py

---
+++ ans/085.py
@@ -0,0 +1,34 @@
+# https://qiita.com/LorseKudos/items/9eb560494862c8b4eb56
+# o(√n)で返り値も整列された状態なので扱いやすい.
+def make_divisors(n):
+    lower_divisors , upper_divisors = [], []
+    i = 1
+    while i*i <= n:
+        if n % i == 0:
+            lower_divisors.append(i)
+            if i != n // i:
+                upper_divisors.append(n//i)
+        i += 1
+    return lower_divisors + upper_divisors[::-1]
+
+
+def main():
+    # data load
+    K = int(input())
+
+    divisors = make_divisors(K)
+
+    result = 0
+    for i, a in enumerate(divisors):
+        for j in range(i, len(divisors)):
+            b = divisors[j]
+            c = K // (a*b)
+            if a <= b <= c and a * b * c == K:
+                result += 1
+
+    # output
+    print(result)
+
+
+if __name__ == '__main__':
+    main()
